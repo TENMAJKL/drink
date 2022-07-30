17d64030a1a4d169b285e3bd702b5a3de29f939b
TENMAJKL <tenmajkl@liskyjidlokod.fun>
2022-07-30 20:51:03 +0200

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+# drink
+
+I have problem with headaches, and most of them are caused due to not-drinking, so I made this little rust app which says you to drink every 30 minutes.
---
+++ src/main.rs
@@ -0,0 +1,43 @@
+// TODO test on linux
+
+use std::{thread, time, env, process::Command};
+
+fn notify() {
+    match env::consts::OS {
+        "macos" => notify_macos(),
+        "linux" => notify_linux(),
+        _ => {
+            println!("Unsuported");
+            return;
+        }
+    }
+}
+
+fn notify_macos() {
+    Command::new("osascript")
+        .arg("-e")
+        .arg("display notification \"Time to drink!\"")
+        .spawn()
+        .expect("Failed to show notification")
+    ;
+}
+
+fn notify_linux() {
+    Command::new("xmessage")
+        .arg("Time to drink!")
+        .spawn()
+        .expect("Failed to show notification")
+    ;
+}
+
+fn main() {
+    if !["linux", "macos"].contains(&env::consts::OS) {
+        println!("Unsuported os.");
+        return;
+    }
+
+    loop {
+        thread::sleep(time::Duration::from_secs(1800));
+        notify();
+    }
+}
